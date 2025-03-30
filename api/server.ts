4db211b722831b64d28641328b8b43c74c17e580
Hilton-J <lhmajola@outlook.com>
2025-03-30 21:04:15 +0200

    Vercel: api folder as vercels entry point

---
+++ api/server.ts
@@ -0,0 +1,31 @@
+import "dotenv/config";
+import express from "express";
+import connectDB from "../backend/config/db";
+import cookieParser from "cookie-parser";
+import authRoutes from "../backend/routes/auth.route";
+import userRoutes from "../backend/routes/user.route";
+import productRoutes from "../backend/routes/product.route";
+import paymentRoutes from "../backend/routes/payment.route";
+import { errorHandler, notFound } from "../backend/middlewares/errorMiddleware";
+
+const app = express();
+
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel!");
+});
+
+app.use("/apiv2/auth", authRoutes);
+app.use("/apiv2/user", userRoutes);
+app.use("/apiv2/product", productRoutes);
+app.use("/apiv2/payment", paymentRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+export default app;
