0362a00dbf205aecebf4a9fcbade4b92dc01b475
Hilton-J <lhmajola@outlook.com>
2025-06-21 18:58:08 +0200

    Minor changes - testing github actions deployment step to render

--- backend/constants/env.const.ts
+++ backend/constants/env.const.ts
@@ -1,3 +1,3 @@
-export const PORT = (process.env.PORT || 5000) as number;
 export const NODE_ENV = process.env.NODE_ENV as string;
+export const PORT = (process.env.PORT || 5000) as number;
 export const MONGO_URI = process.env.MONGO_URI as string;
